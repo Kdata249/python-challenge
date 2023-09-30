8d9e47b68e30f6a106947ff0109dafbddebf8245
Kdata249 <karelklos249@gmail.com>
2023-09-30 19:03:52 -0400

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,14 +3,17 @@ import csv
 
 #vairables
 net_total = 0
+previous_number = 0
 
 
 #lists to store data
 months = []
 profits_losses = []
 changes = []
-greatest_increase = {}
-greatest_decrease = {}
+greatest_increase = {"date": "void",
+    "value": 0}
+greatest_decrease = {"date": "void",
+    "value": 0}
 
 
 budget_data = os.path.join("PyBank", "Resources", "budget_data.csv")
@@ -26,18 +29,32 @@ with open(budget_data) as csvfile:
         months.append(rows[0])
         profits_losses.append(int(rows[1]))
 
+        #calculate changes
+        changes.append(int(rows[1]) - previous_number)
+        previous_number = int(rows[1])
+        
         #calculate total
         net_total = net_total + int(rows[1])
 
+
     #Find greatest increase and decrease in profits
-    for num in profits_losses
-        if num
+    for idx, num in enumerate(changes):
+        if num > greatest_increase["value"]:
+            greatest_increase["value"] = num
+            greatest_increase["date"] = months[idx]
+        if num < greatest_decrease["value"]:
+            greatest_decrease["value"] = num
+            greatest_decrease["date"] = months[idx]
 
     average_change = (profits_losses[-1] - profits_losses[0])/(len(profits_losses) - 1)
     
     print(len(months))
     print(net_total)
     print(average_change)
-    
+    print(changes[2])
+    print(greatest_increase["date"])
+    print(greatest_increase["value"])
+    print(greatest_decrease["date"])
+    print(greatest_decrease["value"])
     
 
